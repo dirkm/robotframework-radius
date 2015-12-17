a4f3efa4e17681c10cfc739a680969267855e712
Michael van Slingerland <michael@deviousops.nl>
2015-12-18 00:34:09 +0100

    add message authenticator

--- RadiusClientLibrary.py
+++ RadiusClientLibrary.py
@@ -17,7 +17,7 @@ class RadiusClientLibrary(object):
         sock.setblocking(0)
         session= { 'sock': sock,
                    'address': address,
-                   'port': port,
+                   'port': int(port),
                    'secret': six.b(str(secret)),
                    'dictionary': dictionary}
         self._cache.register(session, alias=alias)
@@ -41,9 +41,10 @@ class RadiusClientLibrary(object):
                 else:
                   p[k] = v
 
+        p['Message-Authenticator'] =  p.CreateAuthenticator()
         raw = p.RequestPacket()
        
-        session['sock'].sendto(raw,(session['address'],int(session['port'])))
+        session['sock'].sendto(raw,(session['address'],session['port']))
         
     def receive_response(self, alias, code):
         p = None
@@ -51,7 +52,6 @@ class RadiusClientLibrary(object):
         ready = select.select([session['sock']], [], [], 5)
         if ready[0]:
             data, addr = session['sock'].recvfrom(1024)
-            self.builtin.log(len(data))
             p = packet.Packet(secret=session['secret'],packet=data,dict=dictionary.Dictionary(session['dictionary']))
             
             if p.code != getattr(packet,code):
