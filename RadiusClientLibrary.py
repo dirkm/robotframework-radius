4be884533f7c5ec1ea8020cbd1280b2c6e61561f
Michael van Slingerland <michael@deviousops.nl>
2015-12-17 11:15:27 +0100

    secret issue

--- RadiusClientLibrary.py
+++ RadiusClientLibrary.py
@@ -50,11 +50,13 @@ class RadiusClientLibrary(object):
         session['sock'].sendto(raw,self.addr)
         
     def receive_response(self, alias, code):
+        code = int(code)
         p = None
         session = self._cache.switch(alias)
         ready = select.select([self.sock], [], [], 5)
         if ready[0]:
             data, addr = session['sock'].recvfrom(1024)
+            self.builtin.log(len(data))
             p = packet.Packet(secret=session['secret'],packet=data,dict=dictionary.Dictionary(session['dictionary']))
             
             if p.code != code:
