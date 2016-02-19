30f7c2182a47a77330db32ae8d0a68f1ce2b2504
Michael van Slingerland <michael@deviousops.nl>
2016-02-19 17:23:32 +0100

    bumped setup.py to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='robotframework-radius',
-      version='0.1.0',
+      version='0.1.1',
       description='Robotframework RADIUS library',
       author='Michael van Slingerland',
       author_email='michael@deviousops.nl',
